8578c2b2bc4de1e8e439724c109ab04293f3e47f
eunthebear <eun.david@gmail.com>
2017-09-25 22:02:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 function shout(string) {
   return string.toUpperCase();
 }
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase() === string) {
+    return "I can\'t here you!";
+  } else if(string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if(string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
