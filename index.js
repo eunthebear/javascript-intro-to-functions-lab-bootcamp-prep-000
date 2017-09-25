3281696c57eae53ae75503c7197114a96b489dfe
eunthebear <eun.david@gmail.com>
2017-09-25 22:04:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ describe('sayHiToGrandma(string)', function() {
 
 function sayHiToGrandma(string) {
   if(string.toLowerCase() === string) {
-    return "I can\'t here you!";
+    return "I can\'t hear you!";
   } else if(string.toUpperCase() === string) {
     return "YES INDEED!";
   } else if(string === "I love you, Grandma.") {
